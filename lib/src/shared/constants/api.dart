9e76dda2e46334bc65a13536b30a7235111814f0
DanielMartinez <dfernando.martinezg@gmail.com>
2025-10-06 01:49:58 -0500

    feat(DanielMartinez): add app constants

---
+++ lib/src/shared/constants/api.dart
@@ -0,0 +1,2 @@
+const String apiUrl = 'https://api.thecatapi.com/v1';
+const String apiToken = String.fromEnvironment('apiToken');
---
+++ lib/src/shared/constants/layout.dart
@@ -0,0 +1,13 @@
+class UILayout {
+  UILayout._();
+  static final UILayout instance = UILayout._();
+
+  final double layout4 = 4;
+  final double layout8 = 8;
+  final double layout12 = 12;
+  final double layout16 = 16;
+  final double layout20 = 20;
+  final double layout24 = 24;
+  final double layout28 = 28;
+  final double layout32 = 32;
+}
