420383acc622dbb4505714e94821d5ace9f3c7d8
DanielMartinez <dfernando.martinezg@gmail.com>
2025-10-06 22:47:26 -0500

    feat(DanielMartinez): add class with mock response of services

---
+++ test/mocks/mock_response/breed_response.dart
@@ -0,0 +1,9 @@
+class BreedServicesResponse {
+  BreedServicesResponse._();
+  static final BreedServicesResponse instance = BreedServicesResponse._();
+
+  Map<String, String> imagesProviderResponse = <String, String>{
+    'cat_image': 'cat_image_to_test.png',
+    'default': 'default_image_to_test.png',
+  };
+}
